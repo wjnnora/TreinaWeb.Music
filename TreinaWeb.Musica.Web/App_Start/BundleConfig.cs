c91a6d1d70e76f7c6410beddb4a827c53992df3c
wjnnora <wellington_nora94@hotmail.com>
2019-10-29 21:18:34 -0300

    feat(Css): adiciona uma classe própria do css

--- TreinaWeb.Musica.Web/App_Start/BundleConfig.cs
+++ TreinaWeb.Musica.Web/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace TreinaWeb.Musica.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/norastyle.css"));
         }
     }
 }
